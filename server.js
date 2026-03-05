17bf5dbf14537f75c41971eb5f9d6148c555b205
Ellietalbot <tal20011@byui.edu>
2026-03-05 16:41:31 -0700

    created folder structure and basic views

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ const app = express();
 
 const name = process.env.NAME; // <-- NEW
 
-app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`); // <-- UPDATED
-});
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+app.use(express.static('./src/public'));
 
+app.get('/', (req, res) => {
+    const title = 'Welcome Home';
+    res.render('dashboard/dashboard', { title });
+});
+app.get('/family', (req, res) => {
+    const title = 'My Family';
+    res.render('family', { title });
+});
+app.get('/login', (req, res) => {
+        const title = 'My Login';
+    res.render('auth/login', { title });
+});
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
