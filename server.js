4bf783bb74d4f5a4e7f0fe3f5811c2c99b7d5056
Ellietalbot <tal20011@byui.edu>
2026-03-06 16:14:46 -0700

    made family page and polished tasks page

--- server.js
+++ server.js
@@ -2,24 +2,31 @@ import express from 'express';
 
 const app = express();
 
-const name = process.env.NAME; // <-- NEW
-
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 app.use(express.static('./src/public'));
 
 app.get('/', (req, res) => {
-    const title = 'Welcome Home';
+    const title = 'Dashboard';
     res.render('dashboard/dashboard', { title });
 });
-app.get('/family', (req, res) => {
-    const title = 'My Family';
-    res.render('family', { title });
+app.get('/family', (req, res) =>{
+    const title ='My Family';
+    res.render('family/family', {title})
 });
+
 app.get('/login', (req, res) => {
-        const title = 'My Login';
+        const title = 'Login';
     res.render('auth/login', { title });
 });
+app.get('/register', (req, res) => {
+    const title = 'Register';
+    res.render('auth/register', { title });
+});
+app.get('/tasks', (req, res) => {
+    const title = 'Tasks';
+    res.render('tasks/task-list', { title });
+    });
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
